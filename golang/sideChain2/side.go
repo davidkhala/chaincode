27c2bbbd4d572b0c6f3c0f7f2c9aefde0a6cd445
davidliu <david-khala@hotmail.com>
2018-10-22 13:07:07 +0800

    add sideChain2

--- golang/sideChain2/side.go
+++ golang/sideChain2/side.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	. "github.com/davidkhala/fabric-common-chaincode-golang"
-	. "github.com/davidkhala/goutils"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/hyperledger/fabric/protos/peer"
-	"strings"
-	"time"
 )
 
 const (
-	name   = "sideChaincode"
+	name   = "sideChaincode2"
 	mainCC = "mainChain"
 )
 
@@ -32,24 +29,6 @@ func (t SideChaincode) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 	t.Logger.Debug("fcn", fcn, "params", params)
 	var responseBytes []byte
 	switch fcn {
-	case "put":
-		var key1 = UnixMilliSecond(t.GetTxTime()).String()
-		{
-			var args = ArgsBuilder("put")
-			var value = key1
-			args.AppendArg(key1)
-			args.AppendArg(value)
-			t.InvokeChaincode(mainCC, args.Get(), "")
-		}
-		var key2 = UnixMilliSecond(time.Now()).String() + "1"
-		{
-			var args = ArgsBuilder("put")
-			var value = strings.Repeat(key2, 20)
-			args.AppendArg(key2)
-			args.AppendArg(value)
-			t.InvokeChaincode(mainCC, args.Get(), "")
-		}
-		responseBytes = []byte(key1 + "|" + key2)
 	case "get":
 		var args = ArgsBuilder("get")
 		var key = params[0]
