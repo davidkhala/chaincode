8899af08924e91d603976068cb89b738db724f64
davidLiu <david-khala@hotmail.com>
2018-12-17 11:18:15 +0800

    [ToTest] diagnose

--- golang/diagnose/main.go
+++ golang/diagnose/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	. "github.com/davidkhala/fabric-common-chaincode-golang"
+	. "github.com/davidkhala/goutils"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/hyperledger/fabric/protos/peer"
-	"strings"
 )
 
 const (
@@ -17,6 +16,7 @@ type diagnoseChaincode struct {
 }
 
 func (t diagnoseChaincode) Init(stub shim.ChaincodeStubInterface) peer.Response {
+	t.Prepare(stub)
 	t.Logger.Info(" Init ")
 	// GetStatus in Init will timeout request
 	return shim.Success(nil)
@@ -25,65 +25,25 @@ func (t diagnoseChaincode) Init(stub shim.ChaincodeStubInterface) peer.Response 
 
 func (t diagnoseChaincode) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 	t.Prepare(stub)
-	t.Logger.Info("Invoke :")
-
-	fcn, args := stub.GetFunctionAndParameters()
+	fcn, params := stub.GetFunctionAndParameters()
+	t.Logger.Info("Invoke", fcn, params)
+	var response []byte
 	switch fcn {
 	case "worldStates":
 		var states = t.WorldStates("")
-	case "getCreator":
-
+		response = ToJson(states)
+	case "whoami":
+		var cid = NewClientIdentity(stub)
+		response = ToJson(cid)
+	case "get":
+		var key = params[0]
+		response = t.GetState(key)
+	case "put":
+		var key = params[0]
+		var value = params[1]
+		t.PutState(key, []byte(value))
 	}
-
-	var result string
-	if fcn == "worldStates" {
-		kvs, err := WorldStates(stub, creator.Msp);
-		if err != nil {
-			return shim.Error(err.Error())
-		}
-		var stringArray []string;
-		for _, kv := range kvs {
-			jsonString, _ := json.Marshal(kv)
-			jsonElement := string(jsonString)
-			logger.Info(jsonElement)
-			stringArray = append(stringArray, jsonElement)
-		}
-		jsonArray := "[" + strings.Join(stringArray, ",") + "]"
-		return shim.Success([]byte(jsonArray))
-	}
-
-	compositeKey, err := stub.CreateCompositeKey(creator.Msp, []string{args[0]})
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-	if fcn == "read" {
-		if creator.Msp == superMSP {
-			compositeKey, err = stub.CreateCompositeKey(args[1], []string{args[0]})
-			if err != nil {
-				return shim.Error(err.Error())
-			}
-		}
-		resultBytes, err := stub.GetState(compositeKey)
-		if err != nil {
-			return shim.Error(err.Error())
-		}
-		result = string(resultBytes)
-	} else if fcn == "write" {
-		err := stub.PutState(compositeKey, []byte(args[1]))
-		if err != nil {
-			return shim.Error(err.Error())
-		}
-		result = args[0] + "=>" + args[1]
-	} else if fcn == "delete" {
-		err := stub.DelState(compositeKey)
-		if err != nil {
-			return shim.Error(err.Error())
-		}
-		result = args[0]
-	} else {
-		return shim.Error("invalid fcn" + fcn)
-	}
-	return shim.Success([]byte(result))
+	return shim.Success(response)
 }
 
 func main() {
