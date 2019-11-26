16a110589216355656ee6cb6c4883a34d91b4144
david <david-khala@hotmail.com>
2019-11-26 18:05:55 +0800

    prune and add doc

--- nodejs/contracts/stress.js
+++ nodejs/contracts/stress.js
@@ -1,32 +1,75 @@
-const { Contract, Context } = require('fabric-contract-api');
-
-class StressContext extends Context {
-
-	constructor() {
-		super();
-	}
-
-}
+const {Contract, Context} = require('fabric-contract-api');
 
 class StressContract extends Contract {
 
 	constructor() {
-		// Unique namespace when multiple contracts per chaincode file
+		// Supplying a name within the constructor is also option and will default to ''
+		// name could be got by getName()
 		super('stressContract');
 	}
 
+	/**
+	 * This permits contracts to use their own subclass of context
+	 * After this function returns, the chaincodeStub, client identity objects and logger will be injected.
+	 * @override
+	 * @returns {Context}
+	 */
 	createContext() {
-		return new StressContext();
+		this.context = super.createContext();
+		return this.context;
 	}
 
 	/**
-	 * Instantiate to perform any setup of the ledger that might be required.
-	 * @param {Context} ctx the transaction context
+	 *
+	 * @param {string} [label]
 	 */
-	async instantiate(ctx) {
-		// No implementation required with this example
-		// It could be where data migration is performed, if necessary
+	createLogger(label) {
+		this.logger = this.context.logging.getLogger(label); // contract name has been added as tag before label
+	}
 
+	/**
+	 * @override
+	 * @param {Context} context
+	 * @returns {Promise<void>}
+	 */
+	async beforeTransaction(context) {
+		this.createLogger();
+		await super.beforeTransaction(context);
+	}
+
+	/**
+	 * @override
+	 * @param {Context} context
+	 * @param result
+	 * @return {Promise<void>}
+	 */
+	async afterTransaction(context, result) {
+		await super.afterTransaction(context, result);
+	}
+
+	/**
+	 * @override
+	 * @param {Context} context
+	 * @return {Promise<void>}
+	 */
+	async unknownTransaction(context) {
+		await super.unknownTransaction(context);
+	}
+
+	/**
+	 * @override
+	 * @return {string}
+	 */
+	getName() {
+		return super.getName();
+	}
+
+	/**
+	 * 'init' as a default fcn in sdk-node
+	 * @param {Context} context the transaction context
+	 */
+	async init(context) {
+		console.log(context);
 	}
 }
 
